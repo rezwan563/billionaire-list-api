376a9d66ba9997802e406089ce7af1667b10f026
Rezwan <t.rewan17@gmail.com>
2023-03-04 20:26:59 +0600

    Fetched data from billionaire api

--- js/billionaire.js
+++ js/billionaire.js
@@ -1,2 +1,20 @@
 console.log("This is a website")
-const url = "https://forbes400.onrender.com/"
+// const url = "https://forbes400.onrender.com/"
+const loadAllData = () =>{
+    const url = "https://forbes400.onrender.com/api/forbes400/"
+    fetch(url)
+    .then(res => res.json())
+    .then(data => getData(data))
+    .catch(error => console.log(error))
+}
+const getData = data =>{
+    console.log(data)
+    data.forEach(key => {
+        const rank = key.rank;
+        const personName = key.personName;
+        const citizenOf = key.countryOfCitizenship;
+        console.log(key)
+    })
+}
+
+loadAllData()
