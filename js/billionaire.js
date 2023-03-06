d840497f634ef1ac0760609fe0161c28626e5ef8
Rezwan <t.rewan17@gmail.com>
2023-03-06 18:20:16 +0600

    Added estimated wealth and number of companies own with respective share price

--- js/billionaire.js
+++ js/billionaire.js
@@ -27,7 +27,7 @@ function displayData(rank, personName, citizenOf, industry) {
         <td>${personName ? personName : "No data found"}</td>
         <td>${citizenOf ? citizenOf : "No data found"}</td>
         <td>${industry}</td>
-        <td><label for="my-modal-4" class="btn bg-blue-400 hover:bg-blue-700 text-white p-2 rounded-md" onclick=loadData(${rank})>More</label></td>
+        <td><label for="my-modal-4" class="btn bg-white hover:bg-black hover:text-white text-black p-2 rounded-md" onclick=loadData(${rank})>More</label></td>
     </tr>
     `
 }
@@ -87,20 +87,48 @@ const loadData = async (rank) => {
 
 }
 
-const personDetails = (data, rank) =>{
+const personDetails = (data, rank) => {
     // console.log(data)
-    data.forEach(key =>{
+    data.forEach(key => {
         // console.log(key.rank)
         const _isRank = key.rank;
-        if(_isRank === rank){
+        if (_isRank === rank) {
             console.log(key);
             const staticImage = "https://www.google.com/url?sa=i&url=https%3A%2F%2Fdesibooks.co%2Fheadshot-dummy%2F&psig=AOvVaw25aCMfJpE-IJQyXgY3kF--&ust=1678183557958000&source=images&cd=vfe&ved=0CA8QjRxqFwoTCOiPsYiHx_0CFQAAAAAdAAAAABAE"
+            key.financialAssets.forEach(companies => {
+                // console.log(companies.companyName)
+                // console.log(companies.numberOfShares)
+                // console.log(companies.sharePrice)
+                const companyOwn = companies.companyName;
+                const shareOwn = companies.numberOfShares;
+                const sharePrice = companies.sharePrice;
+                document.getElementById("t-head").innerHTML = `
+                    <th>Company</th>
+                    <th>Number of Share</th>
+                    <th>Share Price</th>
+                `
+                document.getElementById("modal-t-body").innerHTML += `
+                <tr>
+                    <td>${companyOwn}</td>
+                    <td>${shareOwn}</td>
+                    <td>${sharePrice}</td>
+                </tr>
+                `
+
+                // console.log(companies.companyName)
+            })
             const personImage = key.person.squareImage;
             const personBio = key.bios.join(" ")
-            document.getElementById("image-box").innerHTML =`
+            document.getElementById("image-box").innerHTML = `
             <img src="${personImage ? personImage : staticImage}" alt="">
             `
+            document.getElementById("text-bio").innerText = "Bio"
+            document.getElementById("wealth-section").innerHTML = `
+            <h1><span class="font-2xl font-semibold">Estimated wealth</span>(in million $)</h1>
+            <p id="person-wealth" class="text-4xl">${key.finalWorth}</p>
+            `
             document.getElementById("person-bio").innerText = personBio;
+            // document.getElementById("person-wealth").innerText = key.finalWorth;
             console.log(`${_isRank} is clicked`)
         }
     })
