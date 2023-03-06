bd60eb04d431851d6a854b7a423668e535b7c94c
Rezwan <t.rewan17@gmail.com>
2023-03-06 16:29:34 +0600

    Added bio of the person inside the modal

--- js/billionaire.js
+++ js/billionaire.js
@@ -93,12 +93,14 @@ const personDetails = (data, rank) =>{
         // console.log(key.rank)
         const _isRank = key.rank;
         if(_isRank === rank){
-            console.log(key.person.squareImage);
+            console.log(key);
             const staticImage = "https://www.google.com/url?sa=i&url=https%3A%2F%2Fdesibooks.co%2Fheadshot-dummy%2F&psig=AOvVaw25aCMfJpE-IJQyXgY3kF--&ust=1678183557958000&source=images&cd=vfe&ved=0CA8QjRxqFwoTCOiPsYiHx_0CFQAAAAAdAAAAABAE"
             const personImage = key.person.squareImage;
+            const personBio = key.bios.join(" ")
             document.getElementById("image-box").innerHTML =`
-            <img src="${personImage}" alt="">
+            <img src="${personImage ? personImage : staticImage}" alt="">
             `
+            document.getElementById("person-bio").innerText = personBio;
             console.log(`${_isRank} is clicked`)
         }
     })
